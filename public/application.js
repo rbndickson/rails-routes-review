f4d9b78765f99f48a647a03f5db987366bad3276
Robin Dickson <robindickson@gmail.com>
2016-06-19 13:06:46 +0100

    Refactor JS - extract logic to functions

--- public/application.js
+++ public/application.js
@@ -1,46 +1,21 @@
 $(function(){
-  $( 'button[type=submit]' ).click( function() {
-    $.post( '/show_answer', { value: $( this ).val() }, function( msg ) {
+  $( "button[type=submit]" ).click( function() {
+    $.post( "/show_answer", { value: $( this ).val() }, function( msg ) {
       msg = JSON.parse( msg );
       $( msg.cell_id ).replaceWith( msg.html );
-      if ( msg.questions_completed == msg.total_questions ) {
-        $( '#quiz' ).css( "margin", "0px" );
-        $( '#quiz' ).prepend( '<div class="alert alert-info">You have finished! You scored ' +
-          msg.correct_amount + ' out of ' + msg.total_questions +
-          '. Choose level for a new game.</div>');
-      }
-      else {
-        $( 'form:first *:input[type!=hidden]:first' ).focus();
-      }
+      checkFinished(msg);
     });
     return false;
   });
 
   $( 'input[type=text]' ).keypress( function ( e ) {
     if ( e.which == 13 ) {
-      $.post( '/check_answer', { user_answer:$( this ).val(), question: this.id }, function( msg ){
+      $.post( '/check_answer', { user_answer: $( this ).val(), question: this.id }, function( msg ) {
         msg = JSON.parse( msg );
         if ( msg.correct === true ) {
           $( msg.cell_id ).replaceWith( msg.html );
-          if (msg.questions_completed == msg.total_questions ) {
-            if (msg.correct_amount == msg.total_questions ) {
-              $( 'td.success' ).effect( "pulsate", { times: 1 }, 1500 );
-              $( '#quiz' ).css( "margin", "0px" );
-              $( '#quiz' ).prepend( '<div class="alert alert-success">Well Done! You scored full marks - ' +
-                msg.correct_amount + ' out of ' + msg.total_questions +
-                '. Maybe time to try the next level!</div>' );
-            }
-            else {
-              $( '#quiz' ).css( "margin", "0px" );
-              $( '#quiz' ).prepend( '<div class="alert alert-info">You have finished! You scored ' +
-                msg.correct_amount + ' out of ' + msg.total_questions + '.</div>' );
-            }
-          }
-          else {
-            $( 'form:first *:input[type!=hidden]:first' ).focus();
-          }
-        }
-        else {
+          checkFinished(msg);
+        } else {
           $( msg.cell_id ).addClass( "pass" );
           $( msg.cell_id ).effect( "bounce", { times: 3 }, "slow" );
         }
@@ -48,4 +23,41 @@ $(function(){
       return false;
     }
   });
+
+  function checkFinished( msg ) {
+    if ( msg.questions_completed === msg.total_questions ) {
+      if ( msg.correct_amount === msg.total_questions ) {
+        fullMarksMessage( msg );
+      } else {
+        gameOverMessage( msg );
+      }
+    } else {
+      $( "form:first *:input[type!=hidden]:first" ).focus();
+    }
+  }
+
+  function gameOverMessage( msg ) {
+    var alert_content = "You have finished! You scored " + score_text( msg ) + ".";
+
+    $( "#quiz" ).prepend( $( "<div></div>", {
+      class: "alert alert-info",
+      text: alert_content
+    }));
+  }
+
+  function fullMarksMessage( msg ) {
+    var alert_content = "Well Done! You scored full marks - " +
+      score_text( msg ) + ". Time to try the next level!";
+
+    $( "td.success" ).effect( "pulsate", { times: 1 }, 1500 );
+    $( "#quiz" ).css( "margin", "0px" );
+    $( "#quiz" ).prepend( $( "<div></div>", {
+      class: "alert alert-success",
+      text: alert_content
+    }));
+  }
+
+  function score_text( msg ) {
+    return msg.correct_amount + " out of " + msg.total_questions;
+  }
 });
